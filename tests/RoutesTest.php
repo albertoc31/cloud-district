1c43f563163cfb331f0cf641af7073499295e562
Alberto <albertoc31@gmail.com>
2021-08-22 20:07:16 +0200

    Testing routes

--- tests/RoutesTest.php
+++ tests/RoutesTest.php
@@ -9,23 +9,26 @@ class RoutesTest extends WebTestCase
     /**
      * @dataProvider provideUrlsMethods
      */
-    public function testRouteIsSuccessful($method, $url)
+    public function testRouteIsSuccessful($method, $url, $status)
     {
-        /** La definición del HHTP_HOST debería establecerse en una variable de entorno */
+        /** La definición del HHTP_HOST debería establecerse en una variable de entorno
+         *  Aquí estamos usando el servidor interno de symfony
+         */
         $client = self::createClient([], [
             'HTTP_HOST' => '127.0.0.1:8000',
         ]);
         $client->request($method, $url);
 
-        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertEquals($status, $client->getResponse()->getStatusCode());
     }
 
     public function provideUrlsMethods()
     {
         return [
-            ['GET','/API/v1/tax/list'],
-            ['GET','/API/v1/product/list/'],
-            // ...
+            ['GET','/API/v1/tax/list', 200],
+            ['GET','/API/v1/product/list', 200],
+            // ['GET','/API/v1/product/new', 405],
+            ['POST','/API/v1/product/new', 422],
         ];
     }
 }
