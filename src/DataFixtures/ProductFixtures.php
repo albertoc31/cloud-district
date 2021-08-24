3e27b63f06d90ca74d0a79604f2a8a792a7fec78
Alberto <albertoc31@gmail.com>
2021-08-24 17:18:34 +0200

    Added 3 more products

--- src/DataFixtures/ProductFixtures.php
+++ src/DataFixtures/ProductFixtures.php
@@ -26,25 +26,47 @@ class ProductFixtures extends Fixture implements DependentFixtureInterface
                 'description' => 'Pack de de 50 botellas de leche',
                 'price' => 150,
                 'price_with_tax' => 165
-            ]
-            , [
+            ], [
                 'name' => 'Saco de arroz',
                 'description' => 'Saco de arroz de 25 kg',
                 'price' => 16,
                 'price_with_tax' => 16.64
+            ], [
+                'name' => 'Chicles de fresa',
+                'description' => 'Paquete de 1000 chicles de fresa',
+                'price' => 10,
+                'price_with_tax' => 12.1
+
+            ], [
+                'name' => 'Caja de yogures naturales',
+                'description' => 'Pack de de 80 yogures naturales',
+                'price' => 80,
+                'price_with_tax' => 88
+            ]
+            , [
+                'name' => 'Saco de garbanzos',
+                'description' => 'Saco de garbanzos de 15 kg',
+                'price' => 18,
+                'price_with_tax' => 18.72
             ]
         ];
 
-        $i = 0;
+        $p = 0;
+        $t = 0;
         foreach ($productsConfig as $productConfig) {
-            $taxId = $this->getReference(TaxFixtures::TAX_REFERENCE . $i);
+            $taxId = $this->getReference(TaxFixtures::TAX_REFERENCE . $t);
             $tax = $manager->getRepository(Tax::class)->find($taxId);
 
             $product = $this->createProduct($productConfig, $tax);
 
             $manager->persist($product);
-            $this->addReference(self::PRODUCT_REFERENCE.$i, $product);
-            $i++;
+            $this->addReference(self::PRODUCT_REFERENCE.$p, $product);
+            if ($t < 2) {
+                $t++;
+            } else {
+                $t = 0;
+            }
+            $p++;
         }
 
         $manager->flush();
